9b10832f472b4c74c489d0c15ceb48a7cdc40b4c
Aaron Bühler <hello.aaronbuehler@gmail.com>
2025-06-07 20:26:47 +0200

    updated azure osc app

---
+++ Azure_osc_sphericalInstrument/src/ofApp.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+
+#include <k4a/k4a.hpp>
+#include <k4abt.h>     // Für k4abt_body_t und k4abt_skeleton_t
+#include <k4abt.hpp>   // Für k4abt::tracker etc.
+
+class ofApp : public ofBaseApp {
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+
+private:
+    k4a::device device;
+    k4a::capture capture;
+    k4a::calibration calibration;
+    k4abt::tracker tracker;
+
+    ofxOscSender sender;
+
+    float scaleFactor = 0.05f;
+    void keyPressed(int key);  // Hinzugefügt
+
+
+    std::vector<k4abt_body_t> currentBodies;  // Richtiger Typ!
+};
--- azurKinect/readme.md
+++
@@ -1,9 +0,0 @@
-Azure SDK:
-https://learn.microsoft.com/en-us/previous-versions/azure/kinect-dk/sensor-sdk-download
-
-Azure Bodytracking SDK:
-https://learn.microsoft.com/en-us/previous-versions/azure/kinect-dk/body-sdk-download
-
-OpenFrameworks Addon
-https://github.com/prisonerjohn/ofxAzureKinect
-
